82ecb1aed639ff911c9292055ef49b24e5be03a8
adamnikk <58403484+adamnik101@users.noreply.github.com>
2023-12-19 15:09:14 +0100

    prettier format

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,67 +1,68 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AlbumService} from "../albums/services/album.service";
-import {Album} from "../albums/interfaces/album";
-import {UserService} from "../user/services/user.service";
-import {Artist} from "../artists/interfaces/artist";
-import {Track} from "../shared/interfaces/track";
-import {From} from "../shared/interfaces/from";
-import {Title} from "@angular/platform-browser";
-import {Subscription} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AlbumService } from '../albums/services/album.service';
+import { Album } from '../albums/interfaces/album';
+import { UserService } from '../user/services/user.service';
+import { Artist } from '../artists/interfaces/artist';
+import { Track } from '../shared/interfaces/track';
+import { From } from '../shared/interfaces/from';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy{
-  albums : Album[] = []
-  recommendedArtists : Artist[] = []
-  recommendedTracks : Track[] = []
-  recentTracks: Track[] = []
+export class HomeComponent implements OnInit, OnDestroy {
+  albums: Album[] = [];
+  recommendedArtists: Artist[] = [];
+  recommendedTracks: Track[] = [];
+  recentTracks: Track[] = [];
   from: From = {
-    id: "",
+    id: '',
     url: '/',
-    name: 'Home'
-  }
-  private sub1!: Subscription
-  private sub2!: Subscription
-  private sub3!: Subscription
-  private sub5!: Subscription
-  public loading: boolean = true
-  constructor(private _albumService: AlbumService,
-              private _userService: UserService,
-              private _titleService: Title) { }
+    name: 'Home',
+  };
+  private sub1!: Subscription;
+  private sub2!: Subscription;
+  private sub3!: Subscription;
+  private sub5!: Subscription;
+  public loading: boolean = true;
+  constructor(
+    private _albumService: AlbumService,
+    private _userService: UserService,
+    private _titleService: Title
+  ) {}
 
   ngOnInit() {
-    this._titleService.setTitle('Home - TREBLE')
+    this._titleService.setTitle('Home - TREBLE');
     this.sub5 = this._userService.getRecentlyPlayedTracks().subscribe({
       next: (tracks) => {
-        this.recentTracks = tracks
-      }
-    })
+        this.recentTracks = tracks;
+      },
+    });
     this.sub1 = this._albumService.getLatest().subscribe({
       next: (response) => {
-        this.albums = response
-      }
-    })
+        this.albums = response;
+      },
+    });
     this.sub2 = this._userService.getRecommendedArtists().subscribe({
       next: (artists) => {
-        this.recommendedArtists = artists
-      }
-    })
+        this.recommendedArtists = artists;
+      },
+    });
     this.sub3 = this._userService.getRecommendedTracks().subscribe({
       next: (tracks) => {
-        this.recommendedTracks = tracks
-        this.loading = false
-      }
-    })
-
+        this.recommendedTracks = tracks;
+        this.loading = false;
+      },
+    });
   }
 
   ngOnDestroy() {
-    this.sub1.unsubscribe()
-    this.sub2.unsubscribe()
-    this.sub3.unsubscribe()
-    this.sub5.unsubscribe()
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
+    this.sub3.unsubscribe();
+    this.sub5.unsubscribe();
   }
 }
--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -2,20 +2,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'duration',
-  standalone: true
+  standalone: true,
 })
 export class DurationPipe implements PipeTransform {
-
   transform(value: number, ...args: unknown[]): unknown {
-
     const seconds: number = Math.floor(value / 1000);
     const minutes: number = Math.floor(seconds / 60);
     const remainingSeconds: number = seconds % 60;
 
-    const formattedMinutes: string = minutes < 10 ? '0' + minutes : String(minutes);
-    const formattedSeconds: string = remainingSeconds < 10 ? '0' + remainingSeconds : String(remainingSeconds);
+    const formattedMinutes: string =
+      minutes < 10 ? '0' + minutes : String(minutes);
+    const formattedSeconds: string =
+      remainingSeconds < 10 ? '0' + remainingSeconds : String(remainingSeconds);
 
     return `${formattedMinutes}:${formattedSeconds}`;
   }
-
 }
