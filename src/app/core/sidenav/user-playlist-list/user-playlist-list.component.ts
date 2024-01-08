79be69882db8c55b5021f9bd965248babce88bb0
adamnikk <58403484+adamnik101@users.noreply.github.com>
2024-01-08 23:49:49 +0100

    improved play pause func from sidenav

--- src/app/core/sidenav/user-playlist-list/user-playlist-list.component.ts
+++ src/app/core/sidenav/user-playlist-list/user-playlist-list.component.ts
@@ -3,6 +3,10 @@ import {Playlist} from "../../../playlists/interfaces/playlist";
 import {CdkDragDrop} from "@angular/cdk/drag-drop";
 import {Track} from "../../../shared/interfaces/track";
 import {DragDropService} from "../../../shared/services/drag-drop.service";
+import {QueueService} from "../../../queue/services/queue.service";
+import {AudioService} from "../../../audio/audio.service";
+import {PlaylistService} from "../../../playlists/services/playlist.service";
+import {From} from "../../../shared/interfaces/from";
 
 @Component({
   selector: 'app-user-playlist-list',
@@ -13,10 +17,23 @@ export class UserPlaylistListComponent {
   @Input('playlist') playlist : Playlist = {} as Playlist
   shouldAddBorder : boolean = false
   track: Track = {} as Track;
-  constructor(public dragDropService: DragDropService) {
+  from: From = {} as From
+  constructor(public dragDropService: DragDropService,
+              protected _queueService: QueueService,
+              private _audioService: AudioService,
+              private _playlistService: PlaylistService) {
   }
-  playPlaylist(playlist: Playlist) {
-
+  playPlaylist(id: string) {
+    this.from = {
+      id : this.playlist.id,
+      name: this.playlist.title,
+      url: '/playlists/' + this.playlist.id
+    }
+    this._playlistService.getPlaylistTracks(id).subscribe({
+      next: (response: any) => {
+        this._queueService.playAllFromIndex(response.tracks, 0, this.from)
+      }
+    })
   }
 
   dropped(id: string, $event: CdkDragDrop<any, any>) {
@@ -31,5 +48,19 @@ export class UserPlaylistListComponent {
 
   removeBorder() {
     this.shouldAddBorder = false
+  }
+
+  pause() {
+    if(this._queueService.currentTrackInfo()) {
+      this._audioService.pause()
+      this._queueService.currentTrackInfo()!.isBeingPlayed = false
+    }
+  }
+
+  continue() {
+    if(this._queueService.currentTrackInfo()) {
+      this._audioService.continue()
+      this._queueService.currentTrackInfo()!.isBeingPlayed = true
+    }
   }
 }
