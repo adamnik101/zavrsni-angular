fcffe09838fdfad6dd28a2f8b43895a6716863e3
adamnikk <58403484+adamnik101@users.noreply.github.com>
2024-01-15 22:23:53 +0100

    create table admin generic

---
+++ src/app/admin/table-admin/table-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-admin.component.html',
+  styleUrl: './table-admin.component.scss'
+})
+export class TableAdminComponent {
+
+}
