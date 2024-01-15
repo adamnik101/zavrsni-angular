93fafeaf1e1c5d020f7876ffd649e9629912e275
adamnikk <58403484+adamnik101@users.noreply.github.com>
2024-01-15 22:07:53 +0100

    update genres in admin

--- src/app/admin/genres/genre-table/genre-table.component.ts
+++ src/app/admin/genres/genre-table/genre-table.component.ts
@@ -1,9 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {PagedResponse} from "../../../shared/interfaces/paged-response";
 import {Genre} from "../../../genre/interfaces/genre";
 import {AdminGenreService} from "../services/admin-genre.service";
 import {MatDialog} from "@angular/material/dialog";
 import {DeleteDialogComponent} from "../../delete-dialog/delete-dialog.component";
+import {MatCheckboxChange} from "@angular/material/checkbox";
+import {SelectionService} from "../../services/selection.service";
+import {DeleteDialogGenericComponent} from "../../delete-dialog-generic/delete-dialog-generic.component";
 
 @Component({
   selector: 'app-genre-table',
@@ -12,9 +15,11 @@ import {DeleteDialogComponent} from "../../delete-dialog/delete-dialog.component
 })
 export class GenreTableComponent {
   @Input('pagedResponse') pagedResponse: PagedResponse<Genre[]> = {} as PagedResponse<Genre[]>
+  @ViewChild('selectAll') selectAllCheckbox!: ElementRef
 
   constructor(private _adminGenreService: AdminGenreService,
-              private _matDialog: MatDialog) {
+              private _matDialog: MatDialog,
+              protected _selectionService: SelectionService) {
   }
   navigateTo(url: string) {
     this._adminGenreService.navigateTo(url)
@@ -33,5 +38,26 @@ export class GenreTableComponent {
 
   openEditDialog(genre: Genre) {
 
+  }
+
+  onSelectAll(event: MatCheckboxChange) {
+    this._selectionService.onAllSelectChange(event, this.pagedResponse.data)
+  }
+
+  onSelectChange(event: MatCheckboxChange, genre: Genre) {
+    this._selectionService.onSingleSelectChange(event, genre.id, this.selectAllCheckbox)
+  }
+
+  openAddDialog() {
+
+  }
+
+  unselectAll() {
+    this._selectionService.removeAll(this.selectAllCheckbox)
+  }
+
+  deleteSelected() {
+    this._matDialog.open(DeleteDialogGenericComponent)
+    console.log(this._selectionService.selectedItems())
   }
 }
