ce944d008f0e9a1cadc3cb7e14e8ab47a002612d
adamnikk <58403484+adamnik101@users.noreply.github.com>
2024-02-03 21:52:53 +0100

    followings loading

--- src/app/user/following/following.component.ts
+++ src/app/user/following/following.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit, Signal, signal, WritableSignal} from '@angular/core';
 import {Artist} from "../../artists/interfaces/artist";
 import {UserService} from "../services/user.service";
 
@@ -7,17 +7,22 @@ import {UserService} from "../services/user.service";
   templateUrl: './following.component.html',
   styleUrls: ['./following.component.scss']
 })
-export class FollowingComponent {
+export class FollowingComponent implements OnInit{
   following : Artist[] = []
-
+  loadedFollowings: WritableSignal<boolean> = signal<boolean>(false)
   constructor(private _userService: UserService) { }
+
   ngOnInit() {
     this._userService.following$.subscribe({
       next: (artists) => {
         this.following = artists
+        if(this.following.length) {
+          this.loadedFollowings.set(true)
+        }
       }
     })
   }
+
   openContextMenu(event: MouseEvent) {
     if(event.button === 2) { // right click, secondary
       console.log('asdasda')
